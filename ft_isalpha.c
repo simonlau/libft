de495c3d770a47d487d9b33e1d6297ffeebb44ee
Simon Lau <simon.lau.sg@gmail.com>
2026-09-23 12:21:44 +0800

    feat: did ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,14 +6,30 @@
 /*   By: simon.lau <simon.lau@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 15:39:55 by simon.lau         #+#    #+#             */
-/*   Updated: 2026/08/06 14:47:51 by simon.lau        ###   ########.fr       */
+/*   Updated: 2026/09/23 11:12:58 by simon.lau        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+
+static int	islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
 int	ft_isalpha(int c)
 {
-	(void)c;
-	return (42);
+	if (isupper(c) || islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
