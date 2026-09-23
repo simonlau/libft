7ff6dc4d4dd86df631f6e0aef1e0b38a37b59c1f
Simon Lau <simon.lau.sg@gmail.com>
2026-09-23 15:02:53 +0800

    feat: did ft_isprint

--- docs/progress.md
+++ docs/progress.md
@@ -3,3 +3,5 @@
 - ft_isdigit
 - ft_isalpha
 - ft_isalnum
+- ft_isascii
+- ft_isprint
--- tests/ft_isprint_test.c
+++ tests/ft_isprint_test.c
@@ -1,10 +1,37 @@
 #include "libft.h"
 #include "registry.h"
+#include "theft.h"
+#include <ctype.h>
+
+static enum theft_trial_res	prop_oracle(struct theft *t, void *arg)
+{
+	int	c;
+	int	result;
+	int	expected;
+
+	(void)t;
+	c = *(const int *)arg;
+	result = ft_isprint(c);
+	expected = isprint(c);
+	if ((expected == 0 && result == 0) || (expected != 0 && result == 1))
+		return (THEFT_TRIAL_PASS);
+	return (THEFT_TRIAL_FAIL);
+}
 
 int	ft_isprint_test(void)
 {
-	/* TODO: implement test for ft_isprint_test */
-	return (EXIT_SUCCESS);
+	enum theft_run_res	res;
+
+	struct theft_run_config cfg = {
+		.prop1 = prop_oracle,
+		.name = __FILE__,
+		.trials = 1000,
+		.type_info = {theft_get_builtin_type_info(THEFT_BUILTIN_int)},
+	};
+	res = theft_run(&cfg);
+	if (res == THEFT_RUN_PASS)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
 }
 
 REGISTER_TEST(0, ft_isprint_test)
