d2ed26671009f4dfbb9f12685546bde8798989ec
Simon Lau <simon.lau.sg@gmail.com>
2026-09-25 13:44:18 +0800

    feat: did ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,16 +6,46 @@
 /*   By: simon.lau <simon.lau@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 15:39:55 by simon.lau         #+#    #+#             */
-/*   Updated: 2026/09/22 16:57:55 by simon.lau        ###   ########.fr       */
+/*   Updated: 2026/09/25 13:36:24 by simon.lau        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+static size_t	strlen(const char *s)
 {
-	(void)dst;
-	(void)src;
-	(void)dstsize;
-	return (42);
+	int	count;
+
+	count = 0;
+	while (*s)
+	{
+		s++;
+		count++;
+	}
+	return (count);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len;
+
+	len = strlen(src);
+	if (size == 0)
+	{
+		return (len);
+	}
+	if (size == 1)
+	{
+		*dst = '\0';
+		return (len);
+	}
+	i = 0;
+	while (i < size - 1 && i < len)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (len);
 }
--- tests/ft_strlcpy_test.c
+++ tests/ft_strlcpy_test.c
@@ -1,10 +1,52 @@
 #include "libft.h"
 #include "registry.h"
+#include "theft.h"
+#include <stdio.h>
+#include <string.h>
+
+static enum theft_trial_res	prop_oracle(struct theft *t, void *arg1, void *arg2)
+{
+	char	*str;
+	char	*actual;
+	char	*expected;
+	size_t	len;
+	size_t	result;
+	size_t	num;
+	size_t	answer;
+
+	(void)t;
+	str = (char *)arg1;
+	num = *(size_t *)arg2;
+	len = strlen(str);
+	len = len > 0 ? num % (len + 1) : 0;
+	actual = calloc(len + 1, sizeof(*actual));
+	expected = calloc(len + 1, sizeof(*expected));
+	result = ft_strlcpy(actual, str, len);
+	answer = strlcpy(expected, str, len);
+	if (result == answer && memcmp(actual, expected, len) == 0)
+	{
+		free(actual);
+		return (THEFT_TRIAL_PASS);
+	}
+	free(actual);
+	return (THEFT_TRIAL_FAIL);
+}
 
 int	ft_strlcpy_test(void)
 {
-	/* TODO: implement test for ft_strlcpy_test */
-	return (EXIT_SUCCESS);
+	enum theft_run_res	res;
+
+	struct theft_run_config cfg = {
+		.prop2 = prop_oracle,
+		.name = __FILE__,
+		.trials = 10,
+		.type_info = {theft_get_builtin_type_info(THEFT_BUILTIN_char_ARRAY),
+			theft_get_builtin_type_info(THEFT_BUILTIN_size_t)},
+	};
+	res = theft_run(&cfg);
+	if (res == THEFT_RUN_PASS)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
 }
 
 REGISTER_TEST(0, ft_strlcpy_test)
