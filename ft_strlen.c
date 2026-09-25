7146d6110cc15db5b39a7b2c41103e0199d263e7
Simon Lau <simon.lau.sg@gmail.com>
2026-09-25 23:45:56 +0800

    fix: change ft_strlen to use size_t

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: simon.lau <simon.lau@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 15:39:55 by simon.lau         #+#    #+#             */
-/*   Updated: 2026/09/23 15:08:20 by simon.lau        ###   ########.fr       */
+/*   Updated: 2026/09/25 23:45:25 by simon.lau        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_strlen(const char *s)
 {
-	int	count;
+	size_t	count;
 
 	count = 0;
 	while (*s)
